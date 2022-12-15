b4a443f3610fa1da1b99d189a828db2589d97311
Amanda Ribeiro <amanda_ribeiro98@yahoo.com.br>
2022-12-14 21:40:19 -0300

    projeto sem edição, apenas conteudo da aula

---
+++ calculadora/src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.div`
+    padding: 20px;
+    border: 1px solid #000000;
+    background-color: #F0E0E4;
+    color: #000000;
+    font-size: 20px;
+    font-weight: 200;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
